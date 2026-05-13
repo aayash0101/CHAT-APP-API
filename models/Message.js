7e04b70e9f049439c1955a27f0486331e6d12eb4
Aayash <dangaayash@gmail.com>
2026-05-13 14:08:10 +0545

    Message model

---
+++ models/Message.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 2000,
+    },
+  },
+  { timestamps: true } // createdAt = the message timestamp you'll display in chat
+);
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
