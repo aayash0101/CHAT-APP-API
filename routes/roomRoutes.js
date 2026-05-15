98786cd74a1a12053a767da2b4461abafb39ae2c
Aayash <dangaayash@gmail.com>
2026-05-15 10:06:43 +0545

    wired

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getRooms, createRoom, getRoomMessages, joinRoom } from "../controllers/roomController.js";
+import { getRooms, createRoom, getRoomMessages, joinRoom, leaveRoom } from "../controllers/roomController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.use(protect);
 router.get("/", getRooms);
 router.post("/", createRoom);
 router.get("/:roomId/messages", getRoomMessages);
-router.post("/:roomId/join", joinRoom); 
+router.post("/:roomId/join", joinRoom);
+router.delete("/:roomId/leave", leaveRoom); 
 
 export default router;
