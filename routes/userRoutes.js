1756547a4bc8bc4bcc5010e2f9ef0812580b64b5
Aayash <dangaayash@gmail.com>
2026-05-15 13:24:45 +0545

    .

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserProfile, updateProfile, uploadAvatar } from "../controllers/userController.js";
-import protect from "../middleware/auth.js";
+import protect from "../middleware/authMiddleware.js";
 import upload from "../config/multer.js";
 
 const router = express.Router();
