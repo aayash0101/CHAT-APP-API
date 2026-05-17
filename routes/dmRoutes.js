4d64a4aef12cdfb833d4cd0077f921899ca54a64
Aayash <dangaayash@gmail.com>
2026-05-17 10:02:23 +0545

    dm routes

---
+++ routes/dmRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getOrCreateDM, getMyDMs, getDMMessages } from "../controllers/dmController.js";
+import protect from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get("/", getMyDMs);
+router.post("/", getOrCreateDM);
+router.get("/:dmId/messages", getDMMessages);
+
+export default router;
