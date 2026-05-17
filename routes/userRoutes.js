1fe8d390bc79ece8177d7d8b54421920a0acfd92
Aayash <dangaayash@gmail.com>
2026-05-17 15:15:43 +0545

    user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,19 @@
 import express from "express";
 import { getUserProfile, updateProfile, uploadAvatar, searchUsers } from "../controllers/userController.js";
 import protect from "../middleware/authMiddleware.js";
-import upload from "../config/multer.js";
+import { uploadAvatar as multerUpload } from "../config/multer.js";
 
 const router = express.Router();
 
-router.use(protect); 
+router.use(protect);
 
-router.get("/search", searchUsers); 
+router.get("/search", searchUsers);
 router.get("/:id", getUserProfile);
 router.put("/profile", updateProfile);
 router.post("/avatar", (req, res, next) => {
-  upload.single("avatar")(req, res, (err) => {
+  multerUpload.single("avatar")(req, res, (err) => {
     if (err) {
-      console.error("Multer/Cloudinary error:", err); 
+      console.error("Multer/Cloudinary error:", err);
       return res.status(500).json({ message: err.message });
     }
     next();
